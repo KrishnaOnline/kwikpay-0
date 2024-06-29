9832c7ed903f676e6d2da825c85326e6035d3c8c
KrishnaOnline <krishnavamshikusuma@gmail.com>
2024-06-29 13:33:22 +0530

    kwikpay monorepo

---
+++ apps/user-app/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [],
+	theme: {
+		extend: {
+			colors: {
+				// 'app': '#0442ED'
+				app: "#0052FF",
+			},
+		},
+	},
+	plugins: [],
+};
